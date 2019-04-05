9c2f115239f258e72465d46245fbc140bd89c64d
Bradon Kanyid <bradon@kanyid.org>
2019-04-05 08:04:05 -0700

    Rev 1.1 of ESP32 Switchboard
    I think this rev is basically ready to go. I could see someone using
    this for real. I still have a lot of software features I wanna add, but
    the hardware is basically complete.
    
    I may come back and add a built-in serial port, as the little breakout
    can be flimsy, but it seems that 3v3 rs232 stuff means SMD components,
    and I've been trying to avoid that to make this easier to DIY.
    
    board:
      Converted footprint to Kicad non-legacy
      Fixed footprint to be HiLetGo ESP-E32, which is narrower than NodeMCU
      Fixed up references on schematic to be in order
      Flipped mistaken footprint on RS232 UART breakout
    
    code:
      Added support for all 12 inputs
      IRQ-based on rising/falling edges
      Added 0.1s timer-based timeout for input switch after input off
        Fixes some bouncing in the signal I didn't expect when powering off

--- esp32-switcher/python/boot.py
+++ esp32-switcher/python/boot.py
@@ -1,16 +1,35 @@
+import gc
+
 from machine import UART
 from machine import Pin
+from machine import Timer
 
 uart = UART(2, 9600)
 uart.init(9600, bits=8, parity=None, stop=1)
 
-p36 = Pin(36, Pin.IN)
-input1 = p36.value()
-while True:
-    lastInput = input1
-    input1 = p36.value()
-    if lastInput != input1:
-        if input1 == 1:
-            uart.write('4*1!')
-        else:
-            uart.write('1*1!')
+inputs = [23, 22, 21, 19, 18, 32, 33, 25, 26, 27, 14, 13]
+pins = [Pin(x, Pin.IN, Pin.PULL_DOWN) for x in inputs]
+off_buffer = [0 for x in inputs]
+off_buffer_timer = Timer(-1)
+
+def set_to_zero(idx, ref):
+    ref[idx] = 0
+
+def pin_callback(p):
+    gc.collect()  # Seems like a decent time to force a clean up...
+
+    input_idx = pins.index(p)
+
+    # Seems that inputs bounce on console off, so let's debounce with a timer
+    if p() == 0:
+        off_buffer[input_idx] = 1
+        off_buffer_timer.init(period=100, mode=Timer.ONE_SHOT, callback=lambda t: set_to_zero(input_idx, off_buffer))
+    else:
+        if off_buffer[input_idx] == 0: # if you're not a bouncing input, then switch
+            serial_write = '{}*1!'.format(input_idx + 1)
+            uart.write(serial_write)
+            print(serial_write)
+        else: # this is a bouncing input that recently turned off, don't switch
+            print('input {} bounced...'.format(input_idx + 1))
+
+irqs = [x.irq(handler=pin_callback, trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING) for x in pins]
