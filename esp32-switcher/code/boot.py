b6f071650644cbc248c2116e4c3b5737b047b587
Bradon Kanyid <rattboi@gmail.com>
2019-08-08 22:26:18 -0700

    Update renders for autoswitch board
    Add multi-output (still hardcoded, but it's a start)
    Naming refactors

--- esp32-switcher/code/boot.py
+++ esp32-switcher/code/boot.py
@@ -7,29 +7,32 @@ from machine import Timer
 uart = UART(2, 9600)
 uart.init(9600, bits=8, parity=None, stop=1)
 
-inputs = [23, 22, 21, 19, 18, 32, 33, 25, 26, 27, 14, 13]
-pins = [Pin(x, Pin.IN, Pin.PULL_DOWN) for x in inputs]
-off_buffer = [0 for x in inputs]
+input_pins = [23, 22, 21, 19, 18, 32, 33, 25, 26, 27, 14, 13]
+pins = [Pin(i, Pin.IN, Pin.PULL_DOWN) for i in input_pins]
+off_buffer = [0 for i in input_pins]
 off_buffer_timer = Timer(-1)
 
-def set_to_zero(idx, ref):
+outputs = [1, 4]
+
+def set_to_zero(ref, idx):
     ref[idx] = 0
 
-def pin_callback(p):
+def pin_callback(pin):
     gc.collect()  # Seems like a decent time to force a clean up...
 
-    input_idx = pins.index(p)
+    input_idx = pins.index(pin)
 
     # Seems that inputs bounce on console off, so let's debounce with a timer
-    if p() == 0:
+    if pin() == 0:
         off_buffer[input_idx] = 1
-        off_buffer_timer.init(period=100, mode=Timer.ONE_SHOT, callback=lambda t: set_to_zero(input_idx, off_buffer))
+        off_buffer_timer.init(period=100, mode=Timer.ONE_SHOT, callback=lambda t: set_to_zero(off_buffer, input_idx))
     else:
         if off_buffer[input_idx] == 0: # if you're not a bouncing input, then switch
-            serial_write = '{}*1!'.format(input_idx + 1)
-            uart.write(serial_write)
-            print(serial_write)
+            for output in outputs:
+                serial_write = '{}*{}!'.format(input_idx + 1, output)
+                uart.write(serial_write)
+                print(serial_write)
         else: # this is a bouncing input that recently turned off, don't switch
             print('input {} bounced...'.format(input_idx + 1))
 
-irqs = [x.irq(handler=pin_callback, trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING) for x in pins]
+irqs = [pin.irq(handler=pin_callback, trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING) for pin in pins]
